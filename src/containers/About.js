14b453ec1ccbe8c6a8f8127a52c2253b4842c1b1
Robert Olsson <robbanolsson@gmail.com>
2018-09-16 23:32:50 +0200

    Lägger in "om förlaget"

--- src/containers/About.js
+++ src/containers/About.js
@@ -4,7 +4,30 @@ import React from 'react'
 
 export default () => (
   <div>
-    <h1>This is what we're all about.</h1>
-    <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
+    <h1>Om förlaget</h1>
+    <p>
+      Argasso är ett barnboksförlag som etablerades i december 2002 och ger ut 18–20 titlar per år.
+      Sedan några år tillbaka publicerar förlaget även 5–10 e-böcker årligen.
+    </p>
+    <p>
+      Argasso bokförlag ger framför allt ut lättlästa böcker för barn och ungdomar
+      (i serien Läsvänligt) och kapitelböcker för mellanåldern. I vår tidigare
+      utgivning finns även ett antal klassiska seriealbum och humoristiska faktaböcker.
+    </p>
+
+    <p>
+      Vi har knutna till oss en mängd etablerade och populära författare som alla behärskar
+      det korta formatet, till exempel Malorie Blackman, Kim Olin, Bali Rai, Nigel Hinton,
+      Anne Cassidy och Robert Swindells.
+    </p>
+    <p>
+      Med lockande omslag och spännande innehåll vill vi att våra böcker ska inspirera till
+      läsning även hos dem som kanske aldrig annars skulle plocka upp en bok. Vi vill att våra
+      böcker ska ha attityd och driv och ge läsaren mersmak. Vi vill att våra böcker ska vara lätta
+      att ta till sig och samtidigt vara fulla av mening, djup, komplikation, humor och kärlek.
+      Vårt mål är att ge läsaren en äkta läsupplevelse, oavsett hur enkel texten är, oavsett hur
+      kort boken är. Vår ambition är att vara en inkörsport till litteraturens värld för unga
+      människor.
+    </p>
   </div>
 )
